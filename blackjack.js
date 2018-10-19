2cc055c6d05284cac64f1fb3b0f6a5e069cab9a7
nikki567 <43977544+nikki567@users.noreply.github.com>
2018-10-18 18:33:27 -0700

    dealer hitting

--- blackjack.js
+++ blackjack.js
@@ -30,6 +30,43 @@ function printCurrentHand(playerHand, dealerHand, dealerCardsToHide) {
   console.log(`\t${formatHand(playerHand, 0)}`.padEnd(tabStop) + `${formatHand(dealerHand, dealerCardsToHide)}`)
 }
 
+function shouldDealerHit(dealerHand, playerHand) {
+  const dealerHandValue = getHandValue(dealerHand)
+  const playerHandValue = getHandValue(playerHand)
+  if (dealerHandValue === 0) return false
+  if (dealerHandValue >= playerHandValue) return false
+  // if (dealerHandValue === playerHandValue) {
+
+
+
+  //   /*
+  //   draw             w   l
+  //   2        19      1 : 13
+  //   3        18      2 : 12
+  //   4        17      3 : 11
+  //   5        16      4 : 10  
+  //   6        15      5 : 9
+  //   7        14      6 : 8
+  //   8        13      7 : 7 ----------
+  //   9        12      8 : 6
+  //   10       11      9 : 5
+  //   10       11      9 : 5
+  //   10       11      9 : 5
+  //   10       11      9 : 5
+  //   11       10      10: 4
+  //   */
+  // }
+  return true
+}
+
+assert.strictEqual(shouldDealerHit(['8♠', '8♥'], ['7♠', '10♥']), true, 'dealer less than player should hit')
+assert.strictEqual(shouldDealerHit(['8♠', 'K♥'], ['7♠', '10♥']), false, 'dealer greater than player should stand')
+assert.strictEqual(shouldDealerHit(['7♠', '10♥'], ['7♠', '10♥']), false, 'dealer equal to player should stand')
+assert.strictEqual(shouldDealerHit(['7♠', '10♥', '8♥'], []), false, 'busted dealer should stand')
+assert.strictEqual(shouldDealerHit(['7♠', '3♥', '8♥'], ['7♠', '3♥', '8♥', '1♥']), true, 'dealer less than player should hit even over 16')
+
+assert.equal(2, 3, 'THIS SHOULD FAIL ON PURPOSE SO WE DO NOT PLAY')
+
 async function run() {
   const deck = makeDeck()
   shuffle(deck)
@@ -42,7 +79,6 @@ async function run() {
     if (shouldHit === true) {
       console.log('Player has hit')
       const card = dealFromTop(deck, 1)[0]
-      console.log(card)
       playerHand.push(card)
     } else {
       console.log('Player stands')
@@ -50,10 +86,27 @@ async function run() {
     }
   }
 
+  //   // Dealer hits if 16 or less. Stands at 17. 
+  while (true) {
+    printCurrentHand(playerHand, dealerHand, 0)
+    const shouldHit = shouldDealerHit(dealerHand)
+    if (shouldHit === true) {
+      console.log('Dealer has hit')
+      const card = dealFromTop(deck, 1)[0]
+      dealerHand.push(card)
+    } else {
+      console.log('Dealer stands')
+      break
+    }
+  }
+
+
+
+  // Hard level: Dealer doesn't hit if player is bust. 
+
   printCurrentHand(playerHand, dealerHand, 0)
   const winner = whoWon(playerHand, dealerHand)
   console.log(`${winner} wins!`)
-
 }
 
 run()
