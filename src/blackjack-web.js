48406294d3aecd0c9ba65c4a64de573fa5822183
Elijah <elijahberlin@yahoo.com>
2018-12-06 18:02:23 -0800

    play again button

--- src/blackjack-web.js
+++ src/blackjack-web.js
@@ -7,6 +7,7 @@ const playerHandContainerID = "playerHandContainer";
 const dealerHandContainerID = "dealerHandContainer";
 const hitButtonID = "hitButton";
 const standButtonID = "standButton";
+const playAgainButtonID = "playAgainButton";
 const winnerMessageID = "winnerMessage";
 const getHandValue = require('./blackjack-deck').getHandValue
 const isBust = require('./blackjack-deck').isBust
@@ -63,6 +64,11 @@ assert.strictEqual(getCardFileName(`2♦`), '2D.png')
 assert.strictEqual(getCardFileName(`10♦`), '10D.png')
 assert.strictEqual(getCardFileName(`A♦`), 'AD.png')
 
+function playAgainButtonHandler() {
+  console.log('New Game')
+  newGame()
+}
+
 function hitButtonHandler() {
   console.log('Player has hit')
   const card = dealFromTop(deck, 1)[0]
@@ -109,11 +115,12 @@ function endGame() {
   const winner = whoWon(playerHand, dealerHand)
   console.log(`${winner} wins!`);
   showWinnerMessage(`${winner} wins!`)
+  playAgainButton.disabled = false;
 }
 
 function showWinnerMessage(message) {
   const winnerMessageElement = document.getElementById(winnerMessageID);
-  winnerMessageElement.innerText = message; 
+  winnerMessageElement.innerText = message;
 }
 clearCards(playerHandContainerID);
 clearCards(dealerHandContainerID);
@@ -135,15 +142,25 @@ dealer play   --- could bust ---> Player Win
 compare hands to determine winner
 
 */
-
-const deck = makeDeck()
-shuffle(deck)
-const playerHand = dealFromTop(deck, 2)
-const dealerHand = dealFromTop(deck, 2)
-showCurrentHand(playerHand, dealerHand, 1)
+var deck
+var playerHand
+var dealerHand
+function newGame() {
+  deck = makeDeck()
+  shuffle(deck)
+  playerHand = dealFromTop(deck, 2)
+  dealerHand = dealFromTop(deck, 2)
+  showCurrentHand(playerHand, dealerHand, 1)
+  hitButton.disabled = false;
+  standButton.disabled = false;
+  playAgainButton.disabled = true;
+  showWinnerMessage(``)
+}
 const hitButton = document.getElementById(hitButtonID);
 const standButton = document.getElementById(standButtonID);
+const playAgainButton = document.getElementById(playAgainButtonID);
 standButton.addEventListener("click", standButtonHandler)
 hitButton.addEventListener("click", hitButtonHandler)
+playAgainButton.addEventListener("click", playAgainButtonHandler)
 
-
+newGame()
